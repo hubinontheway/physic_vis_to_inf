9b27028f81b75098cd7202f2b3a0bf0ace012b57
hubin <hongbin.h@outlook.com>
2026-01-05 12:14:07 +0800

    fix: 修复 models/__init__.py 中引用已删除模块导致的导入错误

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 from .phys_decomp import PhysDecompNet
 from .physics_layer import PhysicsLayer
 from .vit_unet_phys import PhysDecompViTUNet
-from .vis2ir_flow import Vis2IRFlowUNet
+from .vis2ir_flow_pl import Vis2IRFlowLightning
 
 __all__ = [
     "PhysDecompNet",
     "PhysicsLayer",
     "PhysDecompViTUNet",
-    "Vis2IRFlowUNet",
+    "Vis2IRFlowLightning",
 ]
