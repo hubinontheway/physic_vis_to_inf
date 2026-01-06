68bfe954925c03c66095ccc8d8dec52f39c71a25
hubin <1810723114@qq.com>
2026-01-06 21:12:28 +0800

    feat: add run sh file

---
+++ shell/run_eval_flow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Training script for ETRA flat config
+python3 eval_vis2ir_flow.py --run-dir runs/vis2ir_flow_VEDAI_advanced
---
+++ shell/run_train_flow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Training script for ETRA flat config
+python3 train_vis2ir_flow.py --config configs/etra/etra_flat.yml
